8e577a99fd0dd53c9fc6f2ea805d698f28b958be
Bruno Furtado <brunofurmon@gmail.com>
2019-09-05 01:44:14 -0300

    Folder Structure

--- src/application/app.py
+++ src/application/app.py
@@ -1,7 +1,14 @@
-import argparse
+import sys
+
+def processLine(line):
+    print(line, end="")
 
 def main():
-    print('Is GO')
+    
+    # Operates each line from STDIN. Supposing the program is called as following:
+    # python ./src/application/app.py < operations
+    for line in sys.stdin:
+        processLine(line)
 
 if __name__ == '__main__':
     main()
