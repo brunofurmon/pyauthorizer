7a20383892ace0a764f96002f950f489c7bfbdc3
Bruno Furtado <brunofurmon@gmail.com>
2019-09-08 20:43:47 -0300

    Creates a simple test runner for scenario files

---
+++ test/run.py
@@ -0,0 +1,15 @@
+import sys
+import os 
+from src.application import app
+
+if __name__ == '__main__':
+    pathDir = './test/scenarios'
+    scenariosFiles = os.listdir(pathDir)
+
+    for scenarioFilename in scenariosFiles:
+        print('=== Running Scenario file {} ==='.format(scenarioFilename))
+
+        with open(pathDir + '/' + scenarioFilename, 'r') as scenarioFile:
+            sys.stdin = scenarioFile
+            app.main()
+            
